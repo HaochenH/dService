149a882101fc029a05366bc4cf6b55c71b021e98
Haochen Hu <hhuuucc@Hotmail.com>
2023-05-06 17:27:13 -0700

    Revert "Update Landing Page JavaScript"
    This reverts commit 1251be1f073992f0957a2084036b338a52f253cd.

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,50 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // set image directory
-  var imageDir = '../assets/images/slides/';
-  
-  // get image element
-  var img = document.getElementById('auto-change-image');
+    // set image directory
+    var imageDir = '../assets/images/slides/';
+    
+    // get image element
+    var img = document.getElementById('auto-change-image');
+    
+    // set image list
+    var images = [
+      'photo-1639762681057-408e52192e55.png',
+      'photo-1666875753105-c63a6f3bdc86.png',
+      'photo-1510511459019-5dda7724fd87.png',
+      'photo-1517976487492-5750f3195933.png',
+      'photo-1531297484001-80022131f5a1.png',
+      'photo-1625535163131-9d1fc30ea5f5.png',
+      'photo-1653179241553-891d33f05410.png'
+    ];
 
-  // fetch all images in the directory
-  var images = [];
-  fetch(imageDir)
-    .then(response => response.text())
-    .then(html => {
-      var doc = new DOMParser().parseFromString(html, "text/html");
-      var links = doc.querySelectorAll("a");
-      links.forEach(link => {
-        var href = link.getAttribute("href");
-        if (href.endsWith(".jpg") || href.endsWith(".png")) {
-          images.push(href);
-        }
-      });
-    })
-    .catch(error => console.log(error));
-  
-  // preload images
-  var imageObjects = [];
-  for (var i = 0; i < images.length; i++) {
-    var imgObj = new Image();
-    imgObj.src = imageDir + images[i];
-    imageObjects.push(imgObj);
-  }
-  
-  // set index and timer
-  var currentIndex = 0;
-  var timer = setInterval(changeImage, 5500);
-  
-  // change image function
-  function changeImage() {
-    // fade-out
-    img.style.opacity = 0;
-    img.style.transition = 'opacity 0.8s ease-out';
-    setTimeout(function() {
-      // increment index and loop back to start if at end
-      currentIndex = (currentIndex + 1) % images.length;
-      img.src = imageDir + images[currentIndex];
-      // fade-in
-      img.style.opacity = 1;
-      img.style.transition = 'opacity 0.8s ease-in';
-    }, 800);
-  }
-});
+    // preload images
+    var imageObjects = [];
+    for (var i = 0; i < images.length; i++) {
+      var imgObj = new Image();
+      imgObj.src = imageDir + images[i];
+      imageObjects.push(imgObj);
+    }
+    
+    // set index and timer
+    var currentIndex = 0;
+    var timer = setInterval(changeImage, 5500);
+    
+    // change image function
+    function changeImage() {
+      // fade-out
+      img.style.opacity = 0;
+      img.style.transition = 'opacity 0.8s ease-out';
+      setTimeout(function() {
+        // increment index and loop back to start if at end
+        currentIndex = (currentIndex + 1) % images.length;
+        img.src = imageDir + images[currentIndex];
+        // fade-in
+        img.style.opacity = 1;
+        img.style.transition = 'opacity 0.8s ease-in';
+      }, 800);
+    }
+  });
